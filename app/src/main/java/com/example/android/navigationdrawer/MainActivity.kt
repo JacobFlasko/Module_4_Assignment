1bc5773e8b53d1fc5ad42030bb6667ae245eb014
Jacob Flasko <jrf21@students.ptcollege.edu>
2023-05-14 18:42:24 -0400

    this is a commit

--- app/src/main/java/com/example/android/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/android/navigationdrawer/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     @override fun onCreate(savedInstanceState: Bundle?) {
 
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main)
 
